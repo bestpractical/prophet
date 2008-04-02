2230fdf77e15fcaec68bbb2a5608dfa85a7abf79
Chia-liang Kao <clkao@bestpractical.com>
2008-04-02 04:33:47 +0000

    attempt to get best answers
    git-svn-id: svn://s.ly/SVN-PropDB@11381 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/Prophet/Resolver/FromResolutionDB.pm
+++ lib/Prophet/Resolver/FromResolutionDB.pm
@@ -4,6 +4,7 @@ use strict;
 package Prophet::Resolver::FromResolutionDB;
 use base qw/Prophet::Resolver/;
 use Prophet::Change;
+use Digest::MD5 'md5_hex';
 
 sub run {
     my $self = shift;
@@ -14,12 +15,19 @@ sub run {
     my $res = Prophet::Collection->new( handle => $resdb->handle,
                                         type => '_prophet_resolution-'.$conflicting_change->cas_key );
     $res->matching(sub { 1 } );
-    my $answer = $res->as_array_ref->[0] or return;
-#    for my $answer (@{$res->as_array_ref}) {
-        
-#    }
-    
-    warn $answer;
+    return unless @{ $res->as_array_ref};
+
+    my %answer_map;
+    my %answer_count;
+
+    for my $answer (@{$res->as_array_ref}) {
+        my $key = md5_hex(YAML::Syck::Dump($answer->get_props));
+        $answer_map{$key} ||= $answer;
+        $answer_map{$key}++;
+    }
+    my $best = (sort { $answer_map{$b} <=> $answer_map{$a} } keys %answer_map)[0];
+    my $answer = $answer_map{ $best };
+
     my $resolution = Prophet::Change->new_from_conflict($conflicting_change);
     for my $prop_conflict ( @{ $conflicting_change->prop_conflicts } ) {
         $resolution->add_prop_change(
