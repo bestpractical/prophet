748ad08a5b5698ff8678054316b1040ff6d89aff
Jesse Vincent <jesse@bestpractical.com>
2008-04-03 01:47:05 +0000

    r29014@70-5-158-116:  jesse | 2008-04-02 14:12:05 -1000   typo
    git-svn-id: svn://s.ly/SVN-PropDB@11406 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/Prophet/Resolver/AlwaysSource.pm
+++ lib/Prophet/Resolver/AlwaysSource.pm
@@ -7,8 +7,8 @@ use Prophet::Change;
 
 sub run {
     my $self     = shift;
-    my $conflict = shift;
-    return 0 if $conflict->file_op_conflict;
+    my $conflicting_change = shift;
+    return 0 if $conflicting_change->>file_op_conflict;
 
     my $resolution = Prophet::Change->new_from_conflict($conflicting_change);
     return $resolution;
