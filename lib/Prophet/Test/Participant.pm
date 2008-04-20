05009cc3ad27ba49bfbbc03ff45e1b3d4c7d7506
Jesse Vincent <jesse@bestpractical.com>
2008-04-20 00:38:08 +0000

    r29923@31b:  jesse | 2008-04-20 02:32:42 +0200  Store resolutions somewhere useful
    git-svn-id: svn://s.ly/Prophet/trunk@11791 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/Prophet/Test/Participant.pm
+++ lib/Prophet/Test/Participant.pm
@@ -142,7 +142,7 @@ sub dump_state {
 
     my $records  = Prophet::Collection->new( handle => $cli->app_handle->handle, type => 'Scratch' );
     my $merges = Prophet::Collection->new( handle => $cli->app_handle->handle, type => $Prophet::Replica::MERGETICKET_METATYPE );
-    my $resolutions = Prophet::Collection->new( handle => $cli->resdb_handle, type => '_prophet_resolution' );
+    my $resolutions = Prophet::Collection->new( handle => $cli->app_handle->resdb_handle, type => '_prophet_resolution' );
 
     $records->matching( sub       {1} );
     $resolutions->matching( sub {1} );
