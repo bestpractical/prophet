a21b37e20c6260ccbcd27f74e21c652a79b84b8a
Shawn Moore <sartak@bestpractical.com>
2008-07-10 22:14:22 +0000

    r64099@onn:  sartak | 2008-07-10 18:14:16 -0400  Sort lists by numeric luid instead of uuid
    git-svn-id: svn://s.ly/Prophet/trunk@13977 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/Prophet/CLI/Command/Search.pm
+++ lib/Prophet/CLI/Command/Search.pm
@@ -38,7 +38,7 @@ sub run {
     my $search_cb = $self->get_search_callback();
     $records->matching($search_cb);
 
-    for ( sort { $a->uuid cmp $b->uuid } $records->items ) {
+    for ( sort { $a->luid <=> $b->luid } $records->items ) {
         if ( $_->summary_props ) {
             print $_->format_summary . "\n";
         } else {
