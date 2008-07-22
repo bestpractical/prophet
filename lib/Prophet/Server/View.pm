52c5f6f106f946b2f7dbde602cbbd158335c0330
Jesse Vincent <jesse@bestpractical.com>
2008-07-22 17:57:20 +0000

    r40357@31b (orig r14363):  sartak | 2008-07-21 23:59:40 -0700   r64715@onn:  sartak | 2008-07-22 02:59:32 -0400   Add a header to the table
     r40360@31b (orig r14366):  sartak | 2008-07-22 01:20:11 -0700
      r64721@onn:  sartak | 2008-07-22 04:20:00 -0400
      Don't need the type-uuid-propname id on every cell
     
    
    
    git-svn-id: svn://s.ly/Prophet/branches/history@14389 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/Prophet/Server/View.pm
+++ lib/Prophet/Server/View.pm
@@ -21,6 +21,15 @@ template record_table => sub {
         body {
             table {
                 my @items = $records ? $records->items : ();
+                if (@items) {
+                    my @headers = $items[0]->_parse_format_summary;
+                    row {
+                        for (@headers) {
+                            th { $_->{prop} }
+                        }
+                    }
+                }
+
                 for my $record (sort { $a->luid <=> $b->luid } @items) {
                     my $type = $record->type;
                     my $uuid = $record->uuid;
@@ -33,8 +42,7 @@ template record_table => sub {
                             my $prop = $_->{prop};
                             cell {
                                 attr {
-                                    id    => "$type-$uuid-$prop",
-                                    class => $prop,
+                                    class => "prop-$prop",
                                 };
                                 outs $_->{value}
                             }
