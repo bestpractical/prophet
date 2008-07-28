0a722bbcd396b8f412b7f55dc7994005250fe35c
Shawn Moore <sartak@bestpractical.com>
2008-07-28 16:56:21 +0000

    r65326@onn:  sartak | 2008-07-28 12:55:29 -0400  Use "before" instead of explicit override/super
    git-svn-id: svn://s.ly/Prophet/trunk@14581 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/Prophet/CLI/Command/Push.pm
+++ lib/Prophet/CLI/Command/Push.pm
@@ -2,15 +2,13 @@ package Prophet::CLI::Command::Push;
 use Moose;
 extends 'Prophet::CLI::Command::Merge';
 
-override run => sub {
+before run => sub {
     my $self = shift;
 
     die "Please specify a --to.\n" if !$self->has_arg('to');
 
     $self->set_arg(from => $self->app_handle->default_replica_type.":file://".$self->app_handle->handle->fs_root);
     $self->set_arg(db_uuid => $self->app_handle->handle->db_uuid);
-
-    super();
 };
 
 __PACKAGE__->meta->make_immutable;
