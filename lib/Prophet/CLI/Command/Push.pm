f5d9587ab82c5a5397022204b8eb5934cc23f10d
Shawn Moore <sartak@bestpractical.com>
2008-07-28 16:56:00 +0000

    r65324@onn:  sartak | 2008-07-28 12:55:15 -0400  agh "$s\nelf" is bad
    git-svn-id: svn://s.ly/Prophet/trunk@14580 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/Prophet/CLI/Command/Push.pm
+++ lib/Prophet/CLI/Command/Push.pm
@@ -7,9 +7,7 @@ override run => sub {
 
     die "Please specify a --to.\n" if !$self->has_arg('to');
 
-    $self->set_arg(from => $self->app_handle->default_replica_type.":file://".$s
-elf->app_handle->handle->fs_root);
-
+    $self->set_arg(from => $self->app_handle->default_replica_type.":file://".$self->app_handle->handle->fs_root);
     $self->set_arg(db_uuid => $self->app_handle->handle->db_uuid);
 
     super();
