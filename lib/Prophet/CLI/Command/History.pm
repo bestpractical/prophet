4ec69c756cac7f56dd3a0051851e378caf2e73bb
Jesse Vincent <jesse@bestpractical.com>
2008-08-04 05:58:03 +0000

    * Some early tweaks to improve how history is displayed
    git-svn-id: svn://s.ly/Prophet/trunk@14746 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/Prophet/CLI/Command/History.pm
+++ lib/Prophet/CLI/Command/History.pm
@@ -7,16 +7,23 @@ sub run {
     my $self = shift;
 
     my $record = $self->_load_record;
-    print "History for record " . $record->luid . " (" . $record->uuid . ")\n";
-    for my $change ($record->changes) {
+    print "History for record " . $record->luid . " (" . $record->uuid . ")\n\n";
+    for my $changeset ($record->changesets) {
+        my @changes = grep { $_->record_uuid eq $record->uuid }
+                     $changeset->changes;
+
+        my $change = shift @changes;
+        warn "We seem to have multiple changes for a single record for changeset ".$changeset->original_sequence_no .'@'.$changeset->original_source_uuid."\n" if ($changes[0]);
+
         my @prop_changes = $change->prop_changes;
         next if @prop_changes == 0;
 
         # separate each changeset
+        print "Changeset ".$changeset->original_sequence_no .'@'.$changeset->original_source_uuid."\n";
         print "\n";
 
         for my $prop_change (@prop_changes) {
-            print $prop_change->summary, "\n";
+            print "  ".$prop_change->summary, "\n";
         }
     }
 }
