a929341cb30a1983b314a46091b1b519ce119c4c
Shawn Moore <sartak@bestpractical.com>
2008-08-07 19:37:52 +0000

    r68942@onn:  sartak | 2008-08-07 15:37:42 -0400  Reimplement PublishCommand to use rsync directly, and to not put the files into where-you-want/DB-UUID but instead where-you-want
    git-svn-id: svn://s.ly/Prophet/trunk@14900 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/Prophet/CLI/PublishCommand.pm
+++ lib/Prophet/CLI/PublishCommand.pm
@@ -3,35 +3,24 @@ use Moose::Role;
 
 use Path::Class;
 use File::Temp ();
-use File::Rsync;
 
-sub tempdir {
-    my $self = shift;
-    my $tmp = File::Temp::tempdir(CLEANUP => 1);
-    my $uuid = $self->app_handle->handle->db_uuid;
-
-    my $dir = dir($tmp, $uuid);
-    $dir->mkpath;
-
-    return $dir;
-}
+sub tempdir { dir(File::Temp::tempdir(CLEANUP => 1)) }
 
 sub publish_dir {
     my $self = shift;
     my %args = @_;
 
-    my $rsync = File::Rsync->new;
-    $rsync->exec({
-        src       => $args{from},
-        dst       => $args{to},
-        recursive => 1,
-        verbose   => $self->has_arg('verbose'),
-    });
+    my @args;
+    push @args, '--recursive';
+    push @args, '--verbose' if $self->has_arg('verbose');
 
-    warn $_ for $rsync->err;
-    print $_ for $rsync->out;
+    push @args, '--';
 
-    return $rsync;
+    push @args, dir($args{from})->children;
+
+    push @args, $args{to};
+
+    system("rsync", @args);
 }
 
 no Moose::Role;
