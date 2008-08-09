b15927c528a69e33cbddb233ab143f685598563f
Shawn Moore <sartak@bestpractical.com>
2008-08-09 16:43:26 +0000

    r69003@onn:  sartak | 2008-08-09 12:43:17 -0400  Use ChangeSet->as_string in the history command
    git-svn-id: svn://s.ly/Prophet/trunk@14944 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/Prophet/CLI/Command/History.pm
+++ lib/Prophet/CLI/Command/History.pm
@@ -11,28 +11,9 @@ sub run {
 
     print "History for record " . $record->luid . " (" . $record->uuid . ")\n\n";
     for my $changeset ($record->changesets) {
-        my @changes = grep { $_->record_uuid eq $record->uuid }
-                     $changeset->changes;
-
-        my $change = shift @changes;
-        warn "We seem to have multiple changes for a single record for changeset ".$changeset->original_sequence_no .'@'.$changeset->original_source_uuid."\n" if ($changes[0]);
-
-        my @prop_changes = $change->prop_changes;
-        next if @prop_changes == 0;
-
-        # separate each changeset
-        print "Changeset ".$changeset->original_sequence_no .'@'.$changeset->original_source_uuid."\n";
-
-        no warnings 'uninitialized'; # old changesets don't have creator
-        print "by "
-            . $changeset->creator . '@' . $changeset->original_source_uuid
-            ." at " . $changeset->created . "\n";
-
-        for my $prop_change (@prop_changes) {
-            print "  ".$prop_change->summary, "\n";
-        }
-
-        print "\n";
+        print $changeset->as_string(change_filter => sub {
+            shift->record_uuid eq $record->uuid
+        });
     }
 }
 
