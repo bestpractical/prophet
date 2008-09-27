409387456dcd7248e32808d160ffb168ebd1ec1b
Shawn Moore <sartak@bestpractical.com>
2008-09-27 20:47:12 +0000

    r72982@onn:  sartak | 2008-09-27 15:52:50 -0400  pull command
    git-svn-id: svn://s.ly/Prophet/branches/dispatcher@16117 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/Prophet/CLI/Dispatcher.pm
+++ lib/Prophet/CLI/Dispatcher.pm
@@ -209,6 +209,47 @@ on push => sub {
     run('merge', $self, @_);
 };
 
+on pull => sub {
+    my $self = shift;
+    my @from;
+
+    my $from = $self->context->arg('from');
+
+    $self->context->set_arg(db_uuid => $self->cli->handle->db_uuid)
+        unless $self->context->has_arg('db_uuid');
+
+    my %previous_sources = $self->_read_cached_upstream_replicas;
+    push @from, $from
+        if $from
+        && (!$self->context->has_arg('all') || !$previous_sources{$from});
+
+    push @from, keys %previous_sources if $self->context->has_arg('all');
+
+    my @bonjour_replicas = $self->find_bonjour_replicas;
+
+    die "Please specify a --from, --local or --all.\n"
+        unless $from
+            || $self->context->has_arg('local')
+            || $self->context->has_arg('all');
+
+    $self->context->set_arg(to => $self->cli->app_handle->default_replica_type
+            . ":file://"
+            . $self->cli->handle->fs_root );
+
+    for my $from ( @from, @bonjour_replicas ) {
+        print "Pulling from $from\n";
+        #if ( $self->has_arg('all') || $self->has_arg('local') );
+        $self->context->set_arg(from => $from);
+        run("merge", $self, @_);
+        print "\n";
+    }
+
+    if ( $from && !exists $previous_sources{$from} ) {
+        $previous_sources{$from} = 1;
+        $self->_write_cached_upstream_replicas(%previous_sources);
+    }
+};
+
 # catch-all. () makes sure we don't hit the annoying historical feature of
 # the empty regex meaning the last-used regex
 on qr/()/ => sub {
@@ -377,6 +418,73 @@ sub merge_resolver {
         :                     ();
     return $resolver;
 }
+
+=head2 find_bonjour_replicas
+
+Probes the local network for bonjour replicas if the local arg is specified.
+
+Returns a list of found replica URIs.
+
+=cut
+
+sub find_bonjour_replicas {
+    my $self = shift;
+    my @bonjour_replicas;
+    if ( $self->context->has_arg('local') ) {
+        Prophet::App->try_to_require('Net::Bonjour');
+        if ( Prophet::App->already_required('Net::Bonjour') ) {
+            print "Probing for local database replicas with Bonjour\n";
+            my $res = Net::Bonjour->new('prophet');
+            $res->discover;
+            foreach my $entry ( $res->entries ) {
+                if ( $entry->name eq $self->context->arg('db_uuid') ) {
+                    print "Found a database replica on " . $entry->hostname."\n";
+                    my $uri = URI->new();
+                    $uri->scheme( 'http' );
+                    $uri->host($entry->hostname);
+                    $uri->port( $entry->port );
+                    $uri->path('replica/');
+                    push @bonjour_replicas,  $uri->canonical.""; #scalarize
+                }
+            }
+        }
+
+    }
+    return @bonjour_replicas;
+}
+
+=head2 _read_cached_upstream_replicas
+
+Returns a hash containing url => 1 pairs, where the URLs are the replicas that
+have been previously pulled from.
+
+=cut
+
+sub _read_cached_upstream_replicas {
+    my $self = shift;
+    return map { $_ => 1 } $self->cli->handle->_read_cached_upstream_replicas;
+}
+
+=head2 _write_cached_upstream_replicas %replicas
+
+Writes the replica URLs given in C<keys %replicas> to the current Prophet
+repository's upstream replica cache (these replicas will be pulled from when a
+user specifies --all).
+
+=cut
+
+sub _write_cached_upstream_replicas {
+    my $self  = shift;
+    my %repos = @_;
+    return $self->handle->_write_cached_upstream_replicas(keys %repos);
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+
+
+1;
 
 no Moose;
 __PACKAGE__->meta->make_immutable;
