f4ea824449594f6c80db4c7e084128a4315d2917
Shawn Moore <sartak@bestpractical.com>
2008-10-21 18:45:22 +0000

    r74312@onn:  sartak | 2008-10-21 14:45:15 -0400  The entire class-based dispatcher, some short commands don't run classes
    git-svn-id: svn://s.ly/Prophet/branches/class-dispatch@16448 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/Prophet/CLI/Dispatcher.pm
+++ lib/Prophet/CLI/Dispatcher.pm
@@ -1,70 +1,63 @@
 package Prophet::CLI::Dispatcher;
-use strict;
-use warnings;
 use Path::Dispatcher::Declarative -base;
+use Moose;
+with 'Prophet::CLI::Parameters';
 
-# "ticket display $ID" -> "ticket display --id=$ID"
-on qr{ (.*) \s+ ( \d+ | [A-Z0-9]{36} ) $ }x => sub {
-    my %args = @_;
-    $args{cli}->set_arg(id => $2);
-    run($1, %args);
-};
+on [ ['create', 'new'] ]         => command("Create");
+on [ ['show', 'display'] ]       => command("Show");
+on [ ['update', 'edit'] ]        => command("Update");
+on [ ['delete', 'del', 'rm'] ]   => command("Delete");
+on [ ['search', 'list', 'ls' ] ] => command("Search");
 
-on qr{^(\w+)} => sub {
-    my %args = @_;
+on merge   => command("Merge");
+on pull    => command("Pull");
+on publish => command("Publish");
+on server  => command("Server");
+on config  => command("Config");
+on log     => command("Log");
+on shell   => command("Shell");
 
-    my $cmd = __PACKAGE__->resolve_builtin_aliases($1);
-
-    my @possible_classes = (
-        ("Prophet::CLI::Command::" . ucfirst lc $cmd),
-        "Prophet::CLI::Command::NotFound",
-    );
-
-    my $cli = $args{cli};
-
-    for my $class (@possible_classes) {
-        if ($cli->_try_to_load_cmd_class($class)) {
-            return $args{got_command}->($class);
-        }
-    }
-};
-
-on qr{^\s*$} => sub {
-    run(__PACKAGE__->default_command, @_);
-
-};
-
-my %CMD_MAP = (
-    ls      => 'search',
-    new     => 'create',
-    edit    => 'update',
-    rm      => 'delete',
-    del     => 'delete',
-    list    => 'search',
-    display => 'show',
-);
-
-sub resolve_builtin_aliases {
+on export => sub {
     my $self = shift;
-    my @cmds = @_;
+    $self->cli->handle->export_to(path => $self->context->arg('path'));
+};
 
-    if (my $replacement = $CMD_MAP{ lc $cmds[-1] }) {
-        $cmds[-1] = $replacement;
-    }
+on push => sub {
+    my $self = shift;
 
-    @cmds = map { ucfirst lc } @cmds;
+    die "Please specify a --to.\n" if !$self->context->has_arg('to');
 
-    return wantarray ? @cmds : $cmds[-1];
+    $self->context->set_arg(from => $self->cli->app_handle->default_replica_type.":file://".$self->cli->handle->fs_root);
+    $self->context->set_arg(db_uuid => $self->cli->handle->db_uuid);
+    run('merge', $self, @_);
+};
+
+on history => sub {
+    my $self = shift;
+
+    $self->context->require_uuid;
+    my $record = $self->context->_load_record;
+    $self->record($record);
+    print $record->history_as_string;
+};
+
+sub command {
+    my $name = shift;
+
+    return sub {
+        my $self = shift;
+        my $class = $self->class_name($name);
+        $class->new(cli => $self->cli)->run;
+    };
 }
 
-=head2 default_command
+sub class_name {
+    my $command = shift;
+    return "Prophet::CLI::Command::$command";
+}
 
-Returns the "default" command for use when no arguments were specified on the
-command line. In Prophet, it's "shell" but your subclass can change that.
-
-=cut
-
-sub default_command { "shell" }
+__PACKAGE__->meta->make_immutable;
+no Moose;
 
 1;
 
