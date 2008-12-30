060d6a746b0e668dcb3fd532212787e27b3b905a
Shawn Moore <sartak@bestpractical.com>
2008-12-30 01:59:42 +0000

    r78050@onn:  sartak | 2008-12-29 20:59:35 -0500  Remove brackets, they're no longer required
    git-svn-id: svn://s.ly/Prophet/trunk@17417 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/Prophet/Server/Dispatcher.pm
+++ lib/Prophet/Server/Dispatcher.pm
@@ -7,27 +7,27 @@ has server => ( isa => 'Prophet::Server', is => 'rw', weak_ref => 1 );
 sub token_delimiter       {'/'}
 sub case_sensitive_tokens {0}
 
-under ['POST'] => sub {
+under 'POST' => sub {
     on qr'.*' => sub {
         my $self = shift;
         return $self->server->_send_401 if ( $self->server->read_only );
         next_rule;
     };
 
-    under [ 'records'] => sub {
+    under 'records' => sub {
         on qr|^(.*)/(.*)/(.*)$| => sub { shift->server->update_record_prop($1,$2,$3) };
         on qr|^(.*)/(.*).json$| => sub { shift->server->update_record($1,$2) };
         on qr|^(.*).json$|     => sub { shift->server->create_record($1) };
     };
 };
 
-under ['GET'] => sub {
+under 'GET' => sub {
     on qr'^=/prophet/autocomplete' => sub {
         shift->server->show_template('/_prophet_autocompleter') };
     on qr'^static/prophet/(.*)$' => sub { shift->server->send_static_file($1)};
     on qr'replica/+(.*)$' => sub { shift->server->serve_replica($1) };
     on 'records.json' => sub { shift->server->get_record_types };
-    under ['records'] => sub {
+    under 'records' => sub {
         on qr|^(.*)/(.*)/(.*)$| => sub { shift->server->get_record_prop($1,$2,$3); };
         on qr|^(.*)/(.*).json$| => sub { shift->server->get_record($1,$2) };
         on qr|^(.*).json$|      => sub { shift->server->get_record_list($1) };
