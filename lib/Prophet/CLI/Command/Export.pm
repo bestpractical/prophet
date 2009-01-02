2474a4cd13e351cdb1daaf6bf895e2b587cdeb60
Jesse Vincent <jesse@bestpractical.com>
2009-01-02 02:17:46 +0000

    * Update the export command to take a currently undocumented flag to generate an atom feed
    git-svn-id: svn://s.ly/Prophet/trunk@17515 e417ac7c-1bcc-0310-8ffa-8f5827389a85

--- lib/Prophet/CLI/Command/Export.pm
+++ lib/Prophet/CLI/Command/Export.pm
@@ -4,8 +4,31 @@ extends 'Prophet::CLI::Command';
 
 sub run {
     my $self = shift;
+    my $class;
 
-    $self->handle->export_to( path => $self->arg('path') );
+    unless ($self->context->has_arg('path')) {
+        die "You need to specify a --path argument to the 'export' command"."\n";
+    }
+
+    
+    warn $self->context->arg('format');
+    if ($self->context->has_arg('format') && ($self->context->arg('format') eq 'feed') ){
+        $class = 'Prophet::ReplicaFeedExporter';
+    } else {
+        $class = 'Prophet::ReplicaExporter';
+    }
+
+    $self->app_handle->require ($class);
+    warn "Running with class $class";
+
+    my $exporter = $class->new(
+        {   target_path    =>  $self->context->arg('path'),
+            source_replica => $self->app_handle->handle,
+            app_handle     => $self->app_handle
+        }
+    );
+
+    $exporter->export();
 }
 
 __PACKAGE__->meta->make_immutable;
