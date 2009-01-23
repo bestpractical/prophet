71808fb208926e589c00c91ff61dd99c89e4ebde
Christine Spang <spang@bestpractical.com>
2009-01-23 12:41:22 +0200

    clean up Prophet::Test for my own sanity

--- lib/Prophet/Test.pm
+++ lib/Prophet/Test.pm
@@ -20,7 +20,15 @@ use Prophet::CLI;
 
 our $REPO_BASE = File::Temp::tempdir();
 Test::More->import;
-diag( "Replicas can be found in " . $REPO_BASE );
+diag( "Replicas can be found in $REPO_BASE" );
+
+{
+    no warnings 'redefine';
+    require Test::More;
+    sub Test::More::diag {    # bad bad bad # convenient convenient convenient
+        Test::More->builder->diag(@_) if ( $Test::Harness::Verbose || $ENV{'TEST_VERBOSE'} );
+    }
+}
 
 our $EDIT_TEXT = sub { shift };
 do {
@@ -31,9 +39,21 @@ do {
     };
 };
 
+=head2 set_editor($code)
+
+Sets the subroutine that Prophet should use instead of
+C<Prophet::CLI::Command::edit_text> (as this routine invokes an interactive
+editor) to $code.
+
+=cut
+
 sub set_editor {
     $EDIT_TEXT = shift;
 }
+
+=head2 import_extra($class, $args)
+
+=cut
 
 sub import_extra {
     my $class = shift;
@@ -48,20 +68,15 @@ sub import_extra {
         *Test::Builder::plan = sub { };
     }
 
-
-
-    
     delete $ENV{'PROPHET_APP_CONFIG'};
     $ENV{'EMAIL'} = 'nobody@example.com';
 }
 
-{
-    no warnings 'redefine';
-    require Test::More;
-    sub Test::More::diag {    # bad bad bad # convenient convenient convenient
-        Test::More->builder->diag(@_) if ( $Test::Harness::Verbose || $ENV{'TEST_VERBOSE'} );
-    }
-}
+=head2 in_gladiator($code)
+
+Run the given code using L<Devel::Gladiator>.
+
+=cut
 
 sub in_gladiator (&) {
     my $code = shift;
@@ -70,7 +85,7 @@ sub in_gladiator (&) {
     my $types;
     eval { require Devel::Gladiator; };
     if ($@) {
-        warn 'Get Devel::Gladiator from http://code.sixapart.com/svn/Devel-Gladiator/trunk/ and harass sky@crucially.net to CPAN it';
+        warn 'Devel::Gladiator not found';
         return $code->();
     }
     for ( @{ Devel::Gladiator::walk_arena() } ) {
@@ -86,82 +101,44 @@ sub in_gladiator (&) {
 
 }
 
-=head2 run_script SCRIPT_NAME [@ARGS]
+=head2 run_script($script, $args, $stdout, $stderr)
 
-Runs the script SCRIPT_NAME as a perl script, setting the @INC to the same as our caller
+Runs the script $script as a perl script, setting the @INC to the same as
+our caller.
+
+$script is the name of the script to be run (such as 'prophet'). $args is a
+reference to an array of arguments to pass to the script. $stdout and $stderr
+are both optional; if passed in, they will be passed to L<IPC::Run3>'s run3
+subroutine as its $stdout and $stderr args.  Otherwise, this subroutine will
+create scalar references to pass to run3 instead (which are treated as strings
+for STDOUT/STDERR to be written to).
+
+Returns run3's return value and, if no $stdout and $stderr were passed in, the
+STDOUT and STDERR of the script that was run.
 
 =cut
 
 sub run_script {
     my $script = shift;
     my $args = shift || [];
-    my ( $stdout, $stderr );
+    my ( $stdout, $stderr ) = @_;
+    my ( $new_stdout, $new_stderr, $return_stdouterr );
+    if (!ref($stdout) && !ref($stderr)) {
+        ($stdout, $stderr, $return_stdouterr) = (\$new_stdout, \$new_stderr, 1);
+    }
     my @cmd = _get_perl_cmd($script);
 
     local $ENV{IN_PROPHET_TEST_COMMAND} = 1;
 
     #    diag(join(' ', @cmd, @$args));
-    my $ret = run3 [ @cmd, @$args ], undef, \$stdout, \$stderr;
-    Carp::croak $stderr          if $?;
+    my $ret = run3 [ @cmd, @$args ], undef, $stdout, $stderr;
+    # we don't actually want to die if the run command returned an error code
+    # Carp::croak $stderr          if $?;
     #diag( "STDOUT: " . $stdout ) if ($stdout);
     #diag( "STDERR: " . $stderr ) if ($stderr);
 
     #Test::More::diag $stderr;
-    return ( $ret, $stdout, $stderr );
-}
-
-=head2 run_ok SCRIPT_NAME [@ARGS] (<- optional hashref), optional message
-
-Runs the script, checking that it didn't error out.
-
-=cut
-
-sub run_ok {
-    my $script = shift;
-    my $args   = shift if ( ref $_[0] eq 'ARRAY' );
-    my $msg    = (@_) ? shift : '';
-
-    local $Test::Builder::Level = $Test::Builder::Level + 1;
-
-    lives_and {
-        local $Test::Builder::Level = $Test::Builder::Level + 1;
-        my ( $ret, $stdout, $stderr ) = ( run_script( $script, $args ), $msg );
-
-        diag("STDOUT: " . $stdout) if ($stdout);
-        diag("STDERR: " . $stderr) if ($stderr);
-        ok($ret, $msg);
-    };
-}
-
-=head2 _mk_cmp_closure EXPECTED, ERROR
-
-Takes references to an array of expected output lines and an array of
-error messages. Returns a subroutine that takes a list
-of output lines and compares them to its expected output lines,
-storing error messages for lines that don't match in ERROR.
-
-=cut
-
-sub _mk_cmp_closure {
-    my ( $exp, $err ) = @_;
-    my $line = 0;
-
-    $exp = ref($exp) eq 'ARRAY' ? $exp : [$exp];
-
-    sub {
-        my $output = shift;
-        chomp $output;
-        ++$line;
-        unless (@$exp) {
-            push @$err, "$line: got $output";
-            return;
-        }
-        my $item = shift @$exp;
-        push @$err, "$line: got ($output), expect ($item)\n"
-            unless ref($item)
-            ? ( $output =~ m/$item/ )
-            : ( $output eq $item );
-        }
+    return $return_stdouterr ? ( $ret, $$stdout, $$stderr ) : $ret;
 }
 
 our $RUNCNT;
@@ -186,35 +163,121 @@ sub _get_perl_cmd {
     return @cmd;
 }
 
-=head2 is_script_output SCRIPTNAME \@ARGS, \@STDOUT_MATCH, \@STDERR_MATCH, $MSG
+=head2 run_ok($script, $args, $msg)
 
-Runs the script, checking to see that its output matches. Error messages
-for lines that don't match are stored in C<\@STDOUT_MATCH>.
+Runs the script, checking that it didn't error out.
+
+$script is the name of the script to be run (e.g. 'prophet'). $args
+is an optional reference to an array of arguments to pass to the
+script when it is run. $msg is an optional message to print with
+the test. If $args is not specified, you can still pass in
+a $msg.
+
+Returns nothing of interest.
+
+=cut
+
+sub run_ok {
+    my $script = shift;
+    my $args   = shift if ( ref $_[0] eq 'ARRAY' );
+    my $msg    = (@_) ? shift : '';
+
+    local $Test::Builder::Level = $Test::Builder::Level + 1;
+
+    lives_and {
+        local $Test::Builder::Level = $Test::Builder::Level + 1;
+        my ( $ret, $stdout, $stderr ) = run_script( $script, $args );
+        # diag("STDOUT: " . $stdout) if ($stdout);
+        # diag("STDERR: " . $stderr) if ($stderr);
+        ok($ret, $msg);
+    };
+}
+
+=head2 is_script_output($scriptname \@args, \@stdout_match, \@stderr_match, $msg)
+
+Runs $scriptname, checking to see that its output matches.
+
+$args is an array reference of args to pass to the script. $stdout_match and
+$stderr_match are references to arrays of expected lines. $msg is a string
+message to display with the test. $stderr_match and $msg are optional. (As is
+$stdout_match if for some reason you expect your script to have no output at
+all. But that would be silly, wouldn't it?)
+
+Allows regex matches as well as string equality (lines in $stdout_match and
+$stderr_match may be Regexp objects).
 
 =cut
 
 sub is_script_output {
-    my ( $script, $arg, $exp_stdout, $exp_stderr, $msg ) = @_;
+    my ( $script, $args, $exp_stdout, $exp_stderr, $msg ) = @_;
     local $Test::Builder::Level = $Test::Builder::Level + 1;
 
     my $stdout_err = [];
     $exp_stderr ||= [];
-    my @cmd = _get_perl_cmd($script);
 
-    my $ret = run3 [ @cmd, @$arg ], undef, _mk_cmp_closure( $exp_stdout, $stdout_err ),    # stdout
-    _mk_cmp_closure( $exp_stderr, $stdout_err );                    # stderr
+    my $ret = run_script($script, $args,
+        _mk_cmp_closure( $exp_stdout, $stdout_err ),    # stdout
+        _mk_cmp_closure( $exp_stderr, $stdout_err ),    # stderr
+    );
 
-    for my $line(@$exp_stdout) {
+    for my $line (@$exp_stdout) {
         next if !defined $line;
         push @$stdout_err, "got nothing, expected: $line";
     }
 
-    my $test_name = join( ' ', $msg ? "$msg:" : '', $script, @$arg );
+    my $test_name = join( ' ', $msg ? "$msg:" : '', $script, @$args );
     is(scalar(@$stdout_err), 0, $test_name);
+
     if (@$stdout_err) {
         diag( "Different in line: " . join( "\n", @$stdout_err ) );
     }
 }
+
+=head2 _mk_cmp_closure($expected, $error)
+
+$expected is a reference to an array of expected output lines, and
+$error is an array reference for storing error messages.
+
+Returns a subroutine that takes a line of output and compares it
+to the next line in $expected. You can, for example, pass this
+subroutine to L<IPC::Run3>::run3 and it will compare the output
+of the script being run to the expected output. After the script
+is done running, errors will be in $error.
+
+If a line in $expected is a Regexp reference (made with e.g.
+qr/foo/), the subroutine will check for a regexp match rather
+than string equality.
+
+=cut
+
+sub _mk_cmp_closure {
+    my ( $exp, $err ) = @_;
+    my $line = 0;
+
+    $exp = [$exp] if ref($exp) ne 'ARRAY';
+
+    sub {
+        my $output = shift;
+        chomp $output;
+        ++$line;
+        unless (@$exp) {
+            push @$err, "$line: got $output";
+            return;
+        }
+        my $item = shift @$exp;
+        push @$err, "$line: got ($output), expect ($item)\n"
+            unless ref($item) eq 'Regexp'
+            ? ( $output =~ m/$item/ )
+            : ( $output eq $item );
+    }
+}
+
+=head2 run_output_matches($script, $args, $exp_stdout, $exp_stderr, $msg)
+
+A wrapper around L<is_script_output> that also checks to make sure
+the test runs without throwing an exception.
+
+=cut
 
 sub run_output_matches {
     my ( $script, $args, $expected, $stderr, $msg ) = @_;
@@ -225,19 +288,35 @@ sub run_output_matches {
     };
 }
 
+=head2 run_output_matches_unordered($scriptname, \@args, \@exp_stdout)
+
+Runs the given script and checks to make sure that the output
+matches, but the output lines don't necessarily have to be in the same order.
+
+$scriptname is the name of the script to be run (e.g. 'prophet'), $args
+is a reference to an array of arguments to pass to the command, and
+$exp_stdout is a reference to an array of expected output lines.
+
+Line matches are determined through string equality.
+
+=cut
+
 sub run_output_matches_unordered {
     my $cmd = shift;
     my $args = shift;
     my $output = shift;
-    my ($val, $out, $err)  = run_script( $cmd, $args);
+    my ($val, $out, $err)  = run_script( $cmd, $args );
 
     local $Test::Builder::Level = $Test::Builder::Level + 1;
-    is_deeply([sort split(/\n/,$out)], [sort @$output]);
+
+    my @sorted_out = sort split(/\n/,$$out);
+    my @sorted_exp = sort @$output;
+    is_deeply(\@sorted_out, \@sorted_exp);
 }
 
-=head2 repo_path_for $USERNAME
+=head2 repo_path_for($username)
 
-Returns a path on disk for where $USERNAME's replica is stored
+Returns a path on disk for where $username's replica is stored.
 
 =cut
 
@@ -246,9 +325,10 @@ sub repo_path_for {
     return File::Spec->catdir($REPO_BASE => $username);
 }
 
-=head2 repo_uri_for $USERNAME
+=head2 repo_uri_for($username)
 
-Returns a subversion file:// URI for $USERNAME'S replica
+Returns a file:// URI for $USERNAME'S replica (with the correct replica
+type prefix).
 
 =cut
 
@@ -263,11 +343,23 @@ sub repo_uri_for {
     return Prophet::App->default_replica_type . ':file://' . $path;
 }
 
+=head2 replica_uuid
+
+Returns the UUID of the test replica.
+
+=cut
+
 sub replica_uuid {
     my $self = shift;
     my $cli  = Prophet::CLI->new();
     return $cli->handle->uuid;
 }
+
+=head2 database_uuid
+
+Returns the UUID of the test database.
+
+=cut
 
 sub database_uuid {
     my $self = shift;
@@ -275,14 +367,21 @@ sub database_uuid {
     return eval { $cli->handle->db_uuid};
 }
 
+=head2 replica_last_rev
+
+Returns the sequence number of the last change in the test replica.
+
+=cut
+
 sub replica_last_rev {
     my $cli = Prophet::CLI->new();
     return $cli->handle->latest_sequence_no;
 }
 
-=head2 as_user USERNAME CODEREF
+=head2 as_user($username, $coderef)
 
-Run this code block as USERNAME.  This routine sets up the %ENV hash so that when we go looking for a repository, we get the user's repo.
+Run this code block as $username.  This routine sets up the %ENV hash so that
+when we go looking for a repository, we get the user's repo.
 
 =cut
 
@@ -304,17 +403,32 @@ sub as_user {
     return $ret;
 }
 
+=head2 replica_uuid_for($username)
+
+Returns the UUID of the given user's test replica.
+
+=cut
+
 sub replica_uuid_for {
     my $user = shift;
     return $REPLICA_UUIDS{$user};
 }
+
+=head2 database_uuid_for($username)
+
+Returns the UUID of the given user's test database.
+
+=cut
 
 sub database_uuid_for {
     my $user = shift;
     return $DATABASE_UUIDS{$user};
 }
 
-=head2 ensure_new_revisions { CODE }, $numbers_of_new_revisions, $msg
+=head2 ok_added_revisions( { CODE }, $numbers_of_new_revisions, $msg)
+
+Checks that the given code block adds the given number of changes to the test
+replica. $msg is optional and will be printed with the test if given.
 
 =cut
 
@@ -326,9 +440,30 @@ sub ok_added_revisions (&$$) {
     is( replica_last_rev(), $last_rev + $num, $msg );
 }
 
-=head2 serialize_conflict Prophet::Conflict
+=head2 serialize_conflict($conflict_obj)
 
-returns a simple, serialized version of a Prophet::Conflict object suitable for comparison in tests
+Returns a simple, serialized version of a L<Prophet::Conflict> object suitable
+for comparison in tests.
+
+The serialized version is a hash reference containing the following keys:
+    meta => { original_source_uuid => 'source_replica_uuid' }
+    records => { 'record_uuid' =>
+                   { change_type => 'type',
+                     props => { propchange_name => { source_old => 'old_val',
+                                                     source_new => 'new_val',
+                                                     target_old => 'target_val',
+                                                   }
+                              }
+                   },
+                 'another_record_uuid' =>
+                   { change_type => 'type',
+                     props => { propchange_name => { source_old => 'old_val',
+                                                     source_new => 'new_val',
+                                                     target_old => 'target_val',
+                                                   }
+                              }
+                   },
+               }
 
 =cut
 
@@ -351,16 +486,24 @@ sub serialize_conflict {
     return $conflicts;
 }
 
+=head2 serialize_changeset($changeset_obj)
+
+Returns a simple, serialized version of a L<Prophet::ChangeSet> object
+suitable for comparison in tests (a hash).
+
+=cut
+
 sub serialize_changeset {
-    my $cs = shift;
+    my ($cs) = validate_pos( @_, { isa => 'Prophet::ChangeSet' } );
 
     return $cs->as_hash;
 }
 
-=head2 run_command arguments -> stdout
+=head2 run_command($command, @args)
 
-Run the given command using a new L<Prophet::CLI> object. Returns the standard
-output of that command.
+Run the given command with (optionally) the given args using a new
+L<Prophet::CLI> object. Returns the standard output of that command
+in scalar form.
 
 Examples:
 
@@ -378,6 +521,12 @@ sub run_command {
 {
     my $connection = lazy { Prophet::CLI->new->handle };
 
+=head2 load_record($type, $uuid)
+
+Loads and returns a record object for the record with the given type and uuid.
+
+=cut
+
     sub load_record {
         my $type = shift;
         my $uuid = shift;
@@ -390,8 +539,7 @@ sub run_command {
 
 =head2 as_alice CODE, as_bob CODE, as_charlie CODE, as_david CODE
 
-Runs CODE as alice, bob, charlie or david
-
+Runs CODE as alice, bob, charlie or david.
 
 =cut
 
@@ -400,11 +548,11 @@ sub as_bob (&)    { as_user( bob     => shift ) }
 sub as_charlie(&) { as_user( charlie => shift ) }
 sub as_david(&)   { as_user( david   => shift ) }
 
-END {
-    for (qw(alice bob charlie david)) {
+# END {
+#     for (qw(alice bob charlie david)) {
 
-        #     as_user( $_, sub { rmtree [ $ENV{'PROPHET_REPO'} ] } );
-    }
-}
+#         #     as_user( $_, sub { rmtree [ $ENV{'PROPHET_REPO'} ] } );
+#     }
+# }
 
 1;
