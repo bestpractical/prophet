a9f54c6e498a3fe876796ce1b6e15fb9e52998a1
Shawn M Moore <sartak@gmail.com>
2009-02-04 18:55:46 -0500

    De-AH Prophet::Web::FunctionResult

--- lib/Prophet/Web/FunctionResult.pm
+++ lib/Prophet/Web/FunctionResult.pm
@@ -20,19 +20,15 @@ has success => (isa => 'Bool', is => 'rw');
 has message => (isa => 'Str', is => 'rw');
 
 has result => (
-             metaclass => 'Collection::Hash',
              is        => 'rw',
              isa       => 'HashRef[Str]',
              default   => sub { {} },
-             provides  => {
-                 exists    => 'exists',
-                 keys      => 'items',
-                 get       => 'get',
-                 set       => 'set',
-             },
-
 );
 
+sub exists { exists $_[0]->result->{$_[1]} }
+sub items  { keys %{ $_[0]->result } }
+sub get    { $_[0]->result>{$_[1]} }
+sub set    { $_[0]->result->{$_[1]} = $_[2] }
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
