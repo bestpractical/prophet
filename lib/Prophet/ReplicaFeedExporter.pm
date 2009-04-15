10b171677245664d640ad60cce23c4678eced485
Jesse Vincent <jesse@bestpractical.com>
2009-04-15 15:22:09 +0800

    start extracting methods in the replica feed exporter

--- lib/Prophet/ReplicaFeedExporter.pm
+++ lib/Prophet/ReplicaFeedExporter.pm
@@ -6,14 +6,8 @@ my $feed_updated;
 
 sub export {
     my $self = shift;
-    print '<?xml version="1.0" encoding="utf-8"?>' . "\n";
-    print '<feed xmlns="http://www.w3.org/2005/Atom">';
 
-    print tag( 'id' => 'urn:uuid:' . $self->source_replica->uuid );
-    print tag(
-        'title' => 'Prophet feed of ' . $self->source_replica->db_uuid );
-
-    print tag('prophet:latest-sequence', $self->source_replica->latest_sequence_no);
+    print $self->feed_header();
     $self->source_replica->resolution_db_handle->traverse_changesets(
         after    => 0,
         callback => sub {
@@ -32,6 +26,21 @@ sub export {
     print "</feed>";
 }
 
+sub feed_header {
+    my $self = shift;
+
+    return join(
+        "\n", '<?xml version="1.0" encoding="utf-8"?>',
+        '<feed xmlns="http://www.w3.org/2005/Atom">',
+
+        tag( 'id' => 'urn:uuid:' . $self->source_replica->uuid ),
+        tag( 'title' => 'Prophet feed of ' . $self->source_replica->db_uuid
+        ),
+
+        tag( 'prophet:latest-sequence', $self->source_replica->latest_sequence_no )
+    );
+}
+
 sub output_resolution_changeset {
     my $self = shift;
     my $cs   = shift;
@@ -42,16 +51,20 @@ sub output_resolution_changeset {
         sub {
             my $output =
 
-              tag( author => undef, sub { tag( name => $cs->creator ) } )
-              . tag(title => 'Resolution ' . $cs->sequence_no . ' by ' . ( $cs->creator || 'nobody' ) . ' @ ' . $cs->original_source_uuid )
-              . tag(id => 'prophet:' . $cs->original_sequence_no . '@' . $cs->original_source_uuid )
-              . tag( published => $cs->created_as_rfc3339 )
-              . tag( updated   => $cs->created_as_rfc3339 )
-              . '<content type="text">' . "\n"
-              . tag( 'prophet:resolution')
-              . tag( 'prophet:sequence' => $cs->sequence_no )
-              . output_changes($cs)
-              . '</content>';
+                tag( author => undef, sub { tag( name => $cs->creator ) } )
+                . tag(title => 'Resolution '
+                    . $cs->sequence_no . ' by '
+                    . ( $cs->creator || 'nobody' ) . ' @ '
+                    . $cs->original_source_uuid )
+                . tag(
+                id => 'prophet:' . $cs->original_sequence_no . '@' . $cs->original_source_uuid )
+                . tag( published => $cs->created_as_rfc3339 )
+                . tag( updated   => $cs->created_as_rfc3339 )
+                . '<content type="text">' . "\n"
+                . tag('prophet:resolution')
+                . tag( 'prophet:sequence' => $cs->sequence_no )
+                . output_changes($cs)
+                . '</content>';
             return $output;
 
         }
@@ -68,26 +81,27 @@ sub output_changeset {
         sub {
             my $output =
 
-              tag( author => undef, sub { tag( name => $cs->creator ) } )
-              . tag(title => 'Change '
-                  . $cs->sequence_no . ' by '
-                  . ( $cs->creator || 'nobody' ) . ' @ '
-                  . $cs->original_source_uuid )
-              . tag(id => 'prophet:'
-                  . $cs->original_sequence_no . '@'
-                  . $cs->original_source_uuid )
-              . tag( published => $cs->created_as_rfc3339 )
-              . tag( updated   => $cs->created_as_rfc3339 )
-              . '<content type="text">' . "\n"
-              . tag( 'prophet:sequence' => $cs->sequence_no )
-              . ( $cs->is_nullification
-                ? tag( 'prophet:nullifcation' => $cs->is_nullification )
-                : '' )
-              . ( $cs->is_resolution
-                ? tag( 'prophet:resolution' => $cs->is_resolution )
-                : '' )
-              . output_changes($cs)
-              . '</content>';
+                tag( author => undef, sub { tag( name => $cs->creator ) } )
+                . tag(title => 'Change '
+                    . $cs->sequence_no . ' by '
+                    . ( $cs->creator || 'nobody' ) . ' @ '
+                    . $cs->original_source_uuid )
+                . tag(
+                id => 'prophet:' . $cs->original_sequence_no . '@' . $cs->original_source_uuid )
+                . tag( published => $cs->created_as_rfc3339 )
+                . tag( updated   => $cs->created_as_rfc3339 )
+                . '<content type="text">' . "\n"
+                . tag( 'prophet:sequence' => $cs->sequence_no )
+                . (
+                $cs->is_nullification ? tag( 'prophet:nullifcation' => $cs->is_nullification )
+                : ''
+                )
+                . (
+                $cs->is_resolution ? tag( 'prophet:resolution' => $cs->is_resolution )
+                : ''
+                )
+                . output_changes($cs)
+                . '</content>';
             return $output;
 
         }
@@ -102,17 +116,16 @@ sub output_changes {
             'prophet:change',
             undef,
             sub {
-                my $change_data =
-                    tag( 'prophet:type', $change->record_type )
-                  . tag( 'prophet:uuid',        $change->record_uuid )
-                  . tag( 'prophet:change-type', $change->change_type )
-                  . ( $change->is_resolution ? tag('prophet:resolution') : '' )
-                  . (
+                my $change_data
+                    = tag( 'prophet:type',        $change->record_type )
+                    . tag( 'prophet:uuid',        $change->record_uuid )
+                    . tag( 'prophet:change-type', $change->change_type )
+                    . ( $change->is_resolution ? tag('prophet:resolution') : '' )
+                    . (
                     $change->resolution_cas
-                    ? tag( 'prophet:resolution-fingerprint',
-                        $change->resolution_cas )
+                    ? tag( 'prophet:resolution-fingerprint', $change->resolution_cas )
                     : ''
-                  );
+                    );
 
                 foreach my $prop_change ( $change->prop_changes ) {
                     $change_data .= tag(
@@ -120,8 +133,8 @@ sub output_changes {
                         undef,
                         sub {
                             tag( 'prophet:name' => $prop_change->name )
-                              . tag( 'prophet:old' => $prop_change->old_value )
-                              . tag( 'prophet:new' => $prop_change->new_value );
+                                . tag( 'prophet:old' => $prop_change->old_value )
+                                . tag( 'prophet:new' => $prop_change->new_value );
                         }
                     );
 
@@ -147,8 +160,7 @@ sub tag ($$;&) {
     $output .= " " x $depth;
     if ( !$content && !defined $value ) {
         $output .= "<$tag/>\n";
-    }
-    else {
+    } else {
         $output .= "<$tag>";
         if ($value) {
             Prophet::Util::escape_utf8( \$value );
@@ -164,8 +176,6 @@ sub tag ($$;&) {
     $depth--;
     return $output;
 }
-
-
 
 __PACKAGE__->meta->make_immutable;
 no Any::Moose;
