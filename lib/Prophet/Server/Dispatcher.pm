116cc80beb996dac8a3e388c441da8346281e1a1
Jesse Vincent <jesse@bestpractical.com>
2009-04-17 14:57:52 +0800

    make standalone server's "serve the replica" functionality use a facade so it works with sqlite replicas

--- lib/Prophet/Server/Dispatcher.pm
+++ lib/Prophet/Server/Dispatcher.pm
@@ -24,12 +24,56 @@ under { method => 'GET' } => sub {
     on qr'^/=/prophet/autocomplete' => sub {
         shift->server->show_template('/_prophet_autocompleter') };
     on qr'^/static/prophet/(.*)$' => sub { shift->server->send_static_file($1)};
-    on qr'^/replica/+(.*)$' => sub { shift->server->serve_replica($1) };
-    on qr'^/records.json' => sub { shift->server->get_record_types };
+
+   on qr'^/records.json' => sub { shift->server->get_record_types };
     under qr'/records' => sub {
         on qr|^/(.*)/(.*)/(.*)$| => sub { shift->server->get_record_prop($1,$2,$3); };
         on qr|^/(.*)/(.*).json$| => sub { shift->server->get_record($1,$2) };
         on qr|^/(.*).json$|      => sub { shift->server->get_record_list($1) };
+
+    };
+
+    on qr'^/replica(/resolutions)?' => sub {
+        my $self = shift;
+        if ($1 && $1 eq '/resolutions') {
+            $_->metadata->{replica_handle} = $self->server->app_handle->handle->resolution_db_handle;
+        } else {
+            $_->metadata->{replica_handle} = $self->server->app_handle->handle;
+        }
+        next_rule;
+    };
+
+    under qr'^/replica(/resolutions/)?' => sub {
+        on 'replica-version' => sub { shift->server->send_replica_content('1')};
+        on 'replica-uuid' => sub { my $self = shift; $self->server->send_replica_content( $_->metadata->{replica_handle}->uuid ); };
+        on 'database-uuid' => sub { my $self = shift; $self->server->send_replica_content( $_->metadata->{replica_handle}->db_uuid ); };
+        on 'latest-sequence-no' => sub { my $self = shift; $self->server->send_replica_content( $_->metadata->{replica_handle}->latest_sequence_no ); };
+        
+        on 'changesets.idx' => sub {
+            my $self  = shift;
+            my $index = '';
+            $_->metadata->{replica_handle}->traverse_changesets(
+                after=> 0,
+                load_changesets => 0,
+                callback => sub {
+                    my $data            = shift;
+                    my $changeset_index_line = pack( 'Na16NH40',
+                        $data->[0],
+                        Data::UUID->new->from_string( $data->[1]),
+                        $data->[2],
+                        $data->[3]);
+                    $index .= $changeset_index_line;
+                }
+            );
+            $self->server->send_replica_content($index);
+        };
+        on qr|cas/changesets/././(.{40})$| => sub {
+            my $self = shift;
+            my $sha1 = $1;
+            $self->server->send_replica_content($_->metadata->{replica_handle}->fetch_serialized_changeset(sha1 => $sha1));
+        } ;
+
+
     };
 };
 
