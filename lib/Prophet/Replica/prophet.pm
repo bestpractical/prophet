c9403afd2b5527efde9e7288ba91f0c14ac9737c
Alex Vandiver <alexmv@mit.edu>
2009-04-27 13:51:16 -0400

    Add a not-degenerate replica_exists for prophet replicas

--- lib/Prophet/Replica/prophet.pm
+++ lib/Prophet/Replica/prophet.pm
@@ -110,6 +110,10 @@ use constant userdata_dir    => 'userdata';
 use constant changeset_index => 'changesets.idx';
 use constant local_metadata_dir => 'local_metadata';
 
+sub replica_exists {
+    my $self = shift;
+    return -d $self->fs_root ? 1 : 0;
+}
 
 =head1 Replica Format
 
