d29000f6a86cd79c7a2f0ffd77553393376ae2ec
Jesse Vincent <jesse@bestpractical.com>
2009-06-08 23:59:26 -0400

    remove spurious use

--- lib/Prophet/FilesystemReplica.pm
+++ lib/Prophet/FilesystemReplica.pm
@@ -2,7 +2,6 @@ package Prophet::FilesystemReplica;
 use Any::Moose 'Role';
 use File::Spec;use Params::Validate qw(:all);
 use LWP::UserAgent;
-use LWP::ConnCache;
 use JSON;
 use Prophet::Util;
       
