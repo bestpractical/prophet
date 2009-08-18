ff2f7959bfa929663fa27fab078fe2f102e5f17a
Jesse Vincent <jesse@bestpractical.com>
2009-08-17 21:43:39 -0400

    until UUID::Tiny stablizes, a temporary fork

--- lib/Prophet/UUIDGenerator.pm
+++ lib/Prophet/UUIDGenerator.pm
@@ -2,7 +2,9 @@ package Prophet::UUIDGenerator;
 use Any::Moose;
 use MIME::Base64::URLSafe;
 
-use UUID::Tiny;
+#use UUID::Tiny;
+use Prophet::TempUUIDTiny ':std';
+
 
 # uuid_scheme: 1 - v1 and v3 uuids.
 #              2 - v4 and v5 uuids.
@@ -16,9 +18,9 @@ has uuid_scheme => (
 sub create_str {
     my $self = shift;
     if ($self->uuid_scheme == 1 ){
-        return create_UUID_as_string(UUID_V1);
+        return create_uuid_as_string(UUID_V1);
     } elsif ($self->uuid_scheme == 2) {
-        return create_UUID_as_string(UUID_V4);
+        return create_uuid_as_string(UUID_V4);
     }
 }
 
@@ -29,22 +31,22 @@ sub create_string_from_url {
     if ($self->uuid_scheme == 1 ){
         # Yes, DNS, not URL. We screwed up when we first defined it
         # and it can't be safely changed once defined.
-        create_UUID_as_string(UUID_V3, UUID_NS_DNS, $url);
+        create_uuid_as_string(UUID_V3, UUID_NS_DNS, $url);
     } elsif ($self->uuid_scheme == 2) {
-        create_UUID_as_string(UUID_V5, UUID_NS_URL, $url);
+        create_uuid_as_string(UUID_V5, UUID_NS_URL, $url);
     }
 }
 
 sub from_string {
     my $self = shift;
     my $str = shift;
-    return string_to_UUID($str);
+    return string_to_uuid($str);
 }
  
 sub to_string {
     my $self = shift;
     my $uuid = shift;
-    return UUID_to_string($uuid);
+    return uuid_to_string($uuid);
 }
 
 sub from_safe_b64 {
@@ -62,7 +64,7 @@ sub to_safe_b64 {
 sub version {
 	my $self = shift;
 	my $uuid = shift;
-	return version_of_UUID($uuid);
+	return version_of_uuid($uuid);
 }
 
 sub set_uuid_scheme {
