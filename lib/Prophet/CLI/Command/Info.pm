3058d4ad3296b8770ca1505da044bedb699f8b10
Christine Spang <spang@bestpractical.com>
2009-08-21 14:30:56 +0100

    Add resolutions replica info to info command
    It's useful, at least for debugging.

--- lib/Prophet/CLI/Command/Info.pm
+++ lib/Prophet/CLI/Command/Info.pm
@@ -18,11 +18,29 @@ sub run {
 
     $self->print_usage if $self->has_arg('h');
 
-    print "Prophet database: ".$self->handle->url." (@{[ref($self->handle)]})".$/;
-    print "Database UUID:    ".$self->handle->db_uuid.$/;
-    print "Replica UUID:     ".$self->handle->uuid.$/;
-    print "Changesets:       ".$self->handle->latest_sequence_no.$/;
-    print "Known types:      ".join(',', @{$self->handle->list_types} ).$/;
+    print "Records Database\n";
+    print "----------------\n";
+
+    print "Location:      ".$self->handle->url." (@{[ref($self->handle)]})\n";
+    print "Database UUID: ".$self->handle->db_uuid."\n";
+    print "Replica UUID:  ".$self->handle->uuid."\n";
+    print "Changesets:    ".$self->handle->latest_sequence_no."\n";
+    print "Known types:   ".join(',', @{$self->handle->list_types} )."\n\n";
+
+    print "Resolutions Database\n";
+    print "--------------------\n";
+
+    print "Location:      "
+        .$self->handle->resolution_db_handle->url." (@{[ref($self->handle)]})\n";
+    print "Database UUID: "
+        .$self->handle->resolution_db_handle->db_uuid."\n";
+    print "Replica UUID:  "
+        .$self->handle->resolution_db_handle->uuid."\n";
+    print "Changesets:    "
+        .$self->handle->resolution_db_handle->latest_sequence_no."\n";
+    # known types get very unwieldy for resolutions
+    # print "Known types:   "
+    #     .join(',', @{$self->handle->resolution_db_handle->list_types} )."\n";
 }
 
 __PACKAGE__->meta->make_immutable;
