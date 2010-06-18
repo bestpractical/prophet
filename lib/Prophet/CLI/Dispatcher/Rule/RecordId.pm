746a38aa77f8b524f3eb342e1fa15e788e59f1d3
Shawn M Moore <sartak@bestpractical.com>
2010-06-18 16:20:50 -0400

    Tab completion for RecordId rules

--- lib/Prophet/CLI/Dispatcher/Rule/RecordId.pm
+++ lib/Prophet/CLI/Dispatcher/Rule/RecordId.pm
@@ -9,6 +9,32 @@ has '+regex' => (
     default => sub { qr/^$Prophet::CLIContext::ID_REGEX$/i },
 );
 
+has type => (
+    is  => 'ro',
+    isa => 'Str',
+);
+
+sub complete {
+    my $self = shift;
+    my $path = shift->path;
+
+    my $handle = $self->cli->app_handle->handle;
+
+    my @types = $self->type || @{ $handle->list_types };
+
+    my @ids;
+    for my $type (@types) {
+        push @ids,
+            grep { substr($_, 0, length($path)) eq $path }
+            map { ($_->uuid, $_->luid) }
+            @{ $handle->list_records(
+                type         => $type,
+                record_class => $self->cli->record_class,
+            ) };
+    }
+    return @ids;
+}
+
 __PACKAGE__->meta->make_immutable;
 no Any::Moose;
 
